1e722823427dda4f27c287aaa4873fbfad40de37
Stephen Prosser <stephenaprosser@gmail.com>
2013-09-22 12:38:11 -0500

    Added in Capybara and wrote initial test

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Wedding::Application.routes.draw do
   get "static_pages/home"
   get "static_pages/details"
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
