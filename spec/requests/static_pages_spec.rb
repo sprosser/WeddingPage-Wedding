8193e87503fe1348b176551abe681f2b3ac25ee8
Stephen Prosser <stephenaprosser@gmail.com>
2013-09-28 22:43:18 -0500

    Fucking around with home page design

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,45 +1,44 @@
 require 'spec_helper'
 
-# describe "Static pages" do
+describe "Static pages" do
 
-#   subject { page }
+  subject { page }
 
-#   describe "Home page" do
-#     before { visit root_path }
+  describe "Home page" do
+    before { visit root_path }
 
-#     it { should have_content('Wedding Time') }
-#     it { should have_title(full_title('')) }
-#     it { should_not have_title('| Home') }
-#   end
+    it { should have_content('Wedding Time') }
+    it { should have_title(full_title('')) }
+  end
 
-#   describe "Details page" do
-#     before { visit details_path }
+  describe "Details page" do
+    before { visit details_path }
 
-#     it { should have_content('Details') }
-#     it { should have_title(full_title('Details')) }
-#   end
+    it { should have_content('Details') }
+    it { should have_title(full_title('Details')) }
+  end
 
-#   describe "Couple page" do
-#     before { visit couple_path }
+  describe "Couple page" do
+    before { visit couple_path }
 
-#     it { should have_content('Couple') }
-#     it { should have_title(full_title('Couple')) }
-#   end
+    it { should have_content('Amanda and Steve') }
+    it { should have_title(full_title('Couple')) }
+  end
 
-#   describe "Contact page" do
-#     before { visit contact_path }
+  describe "Contact page" do
+    before { visit contact_path }
 
-#     it { should have_content('Contact') }
-#     it { should have_title(full_title('Contact')) }
-#   end
+    it { should have_content('Contact') }
+    it { should have_title(full_title('Contact')) }
+  end
 
-#   describe "News page" do
-#     before { visit news_path }
+  describe "News page" do
+    before { visit news_path }
 
-#     it { should have_content('News') }
-#     it { should have_title(full_title('News')) }
-#   end
-# end
+    it { should have_content('News') }
+    it { should have_title(full_title('News')) }
+  end
+end
 
 
   
